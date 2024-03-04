5cbb2f25e3a32f5a2819002398b9902e1f1bc709
bobaphoever <yannynguyen@gmail.com>
2024-03-04 13:25:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+using Moralis;
+using Moralis.Web3Api.Models;
+
+namespace ConsoleDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.Write("Usage: ConsoleDemo.exe ADDRESS CLIENT_ID");
+                return;
+            }
+
+            string address = "0xBa878d88c71E0091613798C53B6c72aDd9b9A6a7";
+            ChainList chainId = ChainList.eth;
+
+            // Setup Moralis Client
+            MoralisClient.ConnectionData = new Moralis.Models.ServerConnectionData()
+            {
+                ApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImM5NWRmMzc5LWNmOGEtNGY4Yy04YjA2LTYxYzE1MTgzOTJjZiIsIm9yZ0lkIjoiMzgwODQ0IiwidXNlcklkIjoiMzkxMzMzIiwidHlwZUlkIjoiYWJmN2JlNmItZjkxNi00ZDM3LWJjNmEtMGFhNDQyZTVjMTk1IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDkzNTM5MzMsImV4cCI6NDg2NTExMzkzM30.JaIyaoOBhkgAIkIotGI0uK2t7PhaMhc_o1yUtHdyNVk"
+            };
+
+            Task.Run(async () =>
+            {
+                await DisplayCryptoData(address, chainId);
+            }).Wait();
+
+        }
+
+        internal static async Task DisplayCryptoData(string address, ChainList chainId)
+        {
+            try
+            {
+                Console.WriteLine($"For address: {address}...\n");
+
+                // Load native balance for address
+                NativeBalance bal = await MoralisClient.Web3Api.Account.GetNativeBalance(address, chainId);
+
+                double nativeBal = 0;
+
+                double.TryParse(bal.Balance, out nativeBal);
+
+                Console.WriteLine($"Your native balance is {bal.NativeTokenBalance}");
+
+                // Load ERC20 Token List for address
+                List<Erc20TokenBalance> erc20Balnaces = await MoralisClient.Web3Api.Account.GetTokenBalances(address, chainId);
+
+                Console.WriteLine("\n\nYour ERC 20 Tokens:");
+
+                if (erc20Balnaces != null && erc20Balnaces.Count > 0)
+                {
+                    // Print out each token with symbol and balance.
+                    foreach (Erc20TokenBalance tb in erc20Balnaces)
+                    {
+                        Console.WriteLine($"\t{tb.Symbol} - {tb.Name}: {tb.NativeTokenBalance}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\tNone");
+                }
+
+                // Load first 10 NFTs for the address
+                NftOwnerCollection nfts = await MoralisClient.Web3Api.Account.GetNFTs(address, (ChainList)chainId, "", null, 10);
+
+                Console.WriteLine("\n\nYour NFTs:");
+
+                if (nfts != null && nfts.Result.Count > 0)
+                {
+                    // Print out each token with symbol and balance.
+                    foreach (NftOwner nft in nfts.Result)
+                    {
+                        Console.WriteLine($"\t{nft.Name}: {nft.Amount}\n\tMetaData: {nft.Metadata}\n\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\tNone");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+    }
+}
